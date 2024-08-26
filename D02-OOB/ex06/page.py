d736307dfba892ae8a6caaa686c1cb248b56aa47
nuriaurreta <nurreta@gmail.com>
2024-08-26 17:21:18 +0200

    bugs fixed ex06

--- D02-OOB/ex06/page.py
+++ D02-OOB/ex06/page.py
@@ -5,59 +5,52 @@ from elements import Html, Head, Body, Title, Meta, Img, Table, Th, Tr, Td, Ul, 
 class Page:
     def __init__(self, page) -> None:
         if not isinstance(page, Elem):
-            raise ValueError("my_elem must be an instance of Elem class")
+            raise page.ValidationError()
         self.page = page
 
     def is_valid(self) -> bool:
-        return self.validate_element(self)
+        return self.validate_element(self.page)
 
-    def validate_element(self, elem):
-        valid_types = (Html, Head, Body, Title, Meta, Img, Table, Th, Tr, Td, Ul,
-                    Ol, Li, H1, H2, P, Div, Span, Hr, Br, Text)
-        if not isinstance(elem, valid_types):
+    def validate_element(self, page) -> bool:
+        if not (isinstance(page, (Html, Head, Body, Title, Meta, Img, Table, Th, Tr, Td, Ul, Ol, Li,
+                                  H1, H2, P, Div, Span, Hr, Br)) or type(page) == Text):
             return False
-
-        if isinstance(elem, Html):
-            if len(elem.content) != 2 or not isinstance(elem.content[0], Head) or not isinstance(elem.content[1], Body):
-                return False
-        elif isinstance(elem, Head):
-            title_count = 0
-            for child in elem.content:
-                if isinstance(child, Title):
-                    title_count += 1
-                if not self.validate_element(child):
-                    return False
-            if title_count != 1:
-                return False
-        elif isinstance(elem, Body) or isinstance(elem, Div):
-            valid_body_children = (H1, H2, Div, Table, Ul, Ol, Span, Text, P)
-            for child in elem.content:
-                if not isinstance(child, valid_body_children) or not self.validate_element(child):
-                    return False
-        elif isinstance(elem, Title) or isinstance(elem, H1) or isinstance(elem, H2) or isinstance(elem, Li) \
-                or isinstance(elem, Th) or isinstance(elem, Td):
-            if len(elem.content) != 1 or not isinstance(elem.content[0], Text):
-                return False
-        elif isinstance(elem, P):
-            if any(not isinstance(child, Text) for child in elem.content):
-                return False
-        elif isinstance(elem, Span):
-            if any(not isinstance(child, (Text, P)) or not self.validate_element(child) for child in elem.content):
-                return False
-        elif isinstance(elem, Ul) or isinstance(elem, Ol):
-            if len(elem.content) == 0 or any(not isinstance(child, Li) or not self.validate_element(child) for child in elem.content):
-                return False
-        elif isinstance(elem, Tr):
-            th_count = sum(1 for child in elem.content if isinstance(child, Th))
-            td_count = sum(1 for child in elem.content if isinstance(child, Td))
-            if th_count > 0 and td_count > 0:
-                return False
-            if len(elem.content) == 0 or th_count + td_count != len(elem.content):
-                return False
-        elif isinstance(elem, Table):
-            if len(elem.content) == 0 or any(not isinstance(child, Tr) or not self.validate_element(child) for child in elem.content):
-                return False
-        return True
+        if type(page) == Text or isinstance(page, Meta):
+            return True
+        if isinstance(page, Html) and len(page.content) == 2 \
+                and type(page.content[0]) == Head and type(page.content[1]) == Body:
+            if (all(self.validate_element(el) for el in page.content)):
+                return True
+        elif isinstance(page, Head) and [isinstance(el, Title) for el in page.content].count(True) == 1:
+            if (all(self.validate_element(el) for el in page.content)):
+                return True
+        elif isinstance(page, (Body, Div)) and \
+                all([isinstance(el, (H1, H2, Div, Table, Ul, Ol, Span)) or
+                    type(el) == Text for el in page.content]):
+            if (all(self.validate_element(el) for el in page.content)):
+                return True
+        elif isinstance(page, (Title, H1, H2, Li, Th, Td)) and \
+                len(page.content) == 1 and type(page.content[0]) == Text:
+            return True
+        elif isinstance(page, P) and \
+                all([isinstance(el, Text) for el in page.content]):
+            return True
+        elif isinstance(page, Span) and \
+                all([isinstance(el, (Text, P)) for el in page.content]):
+            if (all(self.validate_element(el) for el in page.content)):
+                return True
+        elif isinstance(page, (Ul, Ol)) and len(page.content) > 0 and \
+                all([isinstance(el, Li) for el in page.content]):
+            if (all(self.validate_element(el) for el in page.content)):
+                return True
+        elif isinstance(page, Tr) and len(page.content) > 0 and\
+                all([isinstance(el, (Th, Td)) for el in page.content]) and \
+                all([type(el) == type(page.content[0]) for el in page.content]):
+            return True
+        elif isinstance(page, Table) and \
+                all([isinstance(el, Tr) for el in page.content]):
+            return True
+        return False
 
     def __str__(self) -> str:
         result = ""
@@ -68,7 +61,4 @@ class Page:
 
     def write_to_file(self, filename):
         with open(filename, 'w') as f:
-            f.write(str(self))
-
-if __name__ == '__main__':
-    pass
+            f.write(str(self.__str__()))
